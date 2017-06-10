5b4b3c0452aedc92b3638fa19e992acd41614abe
gisang <gisangkim@gmail.com>
2017-06-10 18:27:55 +0900

    first master

---
+++ master/src/main/java/com/modelncode/crudpattern/Application.java
@@ -0,0 +1,12 @@
+package com.modelncode.crudpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
