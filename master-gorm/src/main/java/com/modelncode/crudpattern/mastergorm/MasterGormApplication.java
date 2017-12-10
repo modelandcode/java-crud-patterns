3446175478f430a9f3fde4f9d74766021a12f311
gisang <poalris1>
2017-12-10 18:58:52 +0900

    Add Master-Detail-JPA 구현 소스

---
+++ master-gorm/src/main/java/com/modelncode/crudpattern/mastergorm/MasterGormApplication.java
@@ -0,0 +1,12 @@
+package com.modelncode.crudpattern.mastergorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterGormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterGormApplication.class, args);
+	}
+}
